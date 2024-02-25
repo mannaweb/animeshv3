e0e6df4386f42dfbb11f0d6878865612afe8fda0
Symlinktech <symlinktech@gmail.com>
2024-02-25 20:47:35 +0530

    up to dated

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -34,11 +34,13 @@ export default function Hero() {
     >
       <div className="-mt-[112px] w-full">
         <div className="mx-auto max-w-7xl">
+       
           <AnimatePresence>
             <FadeUp key="title-main" duration={0.6}>
               <h1 className="bg-gradient-to-r from-teal-500 to-tera-500 bg-clip-text py-2 text-5xl font-bold text-transparent dark:from-teal-200 dark:to-teal-500 sm:text-6xl md:text-7xl xl:text-8xl">
                 Animesh Manna
               </h1>
+            
               <span className="text-xl font-semibold text-zinc-900 dark:text-zinc-100 md:text-3xl">
                 Backend and React Native developer
               </span>
@@ -55,10 +57,12 @@ export default function Hero() {
                 </span>{" "}
                 , I turn vision into reality with code and design.
               </div>
+             
             </FadeUp>
           </AnimatePresence>
         </div>
       </div>
+      <div className="gangnamstyle"></div>
     </motion.section>
   );
 }
